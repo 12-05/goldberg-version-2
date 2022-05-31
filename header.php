7b554c3a2c717b5ec8a1e76446ff9b1e70bb1e80
Magnus Westhofen <m.westhofen@12-05.de>
2022-05-31 17:51:03 +0200

    roboto webfont

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
         <!--- display wordpress title tag -->
         <title><?php wp_title('|', true, 'right'); ?></title>
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/png">
 
 
     </head>
