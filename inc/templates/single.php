363af677a32a7b3cc49473e09d384894415a3f90
Magnus Westhofen <m.westhofen@12-05.de>
2022-06-01 16:45:07 +0200

    test

--- inc/templates/single.php
+++ inc/templates/single.php
@@ -22,8 +22,8 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php
 		the_title(
-			sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h2>'
+			sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h1>'
 		);
 		?>
 
