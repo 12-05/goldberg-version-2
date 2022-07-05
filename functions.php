55b488ecdaced52e25efd545656d21c83a975643
Magnus Westhofen <m.westhofen@12-05.de>
2022-07-05 16:40:24 +0200

    test

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
             add_filter( 'show_admin_bar', '__return_false' );
             add_action( 'after_setup_theme', array($this, 'add_editor_styles'));
             add_action('widgets_init', array($this, 'register_side_bar'));
-            add_action( 'add_editor_scripts', array($this, 'load_editors_scripts'));
+            add_action( 'enqueue_block_editor_assets', array($this, 'load_editors_scripts'));
             $this->load_inc();  
         }
 
@@ -75,7 +75,6 @@
             wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/compiled/custom.js', array('jquery') );
             wp_enqueue_script( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery') );
         }
-
         public function register_side_bar() {
             register_sidebar( array(
                 'name'          => __( 'Sidebar2', 'goldberg' ),
