a952d272b72e340ca35cf559f309618861678bfd
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-17 11:39:44 +0200

    test

--- single-office.php
+++ single-office.php
@@ -9,7 +9,24 @@
     <h2>Unser Büro</h2>
     <div class="content">
         <?php the_field('beschreibung');?>
+        <div style="float:right;padding:1rem;background:#EEE">
+                            <h3><?php echo get_the_title($id);?></h3>
+                            <p>
+                                Büro <?php echo get_field('stadt');?><br />
+                                <?php echo get_field('strasse');?><br />
+                                <?php echo get_field('postleitzahl');?> <?php echo get_field('stadt');?>
+                            </p>
+                            <p>
+                                Telefon: <?php echo get_field('telefon');?><br />
+                                Telefax: <?php echo get_field('fax');?>
+                            </p>
+
+                            <p>
+                                E-Mail: <a href="mailto:<?php echo get_field('email');?>"><?php echo get_field('email');?></a>
+                            </p>
     </div>
+    </div>
+
 </section>
 
     
