b5b7bd4faeeb33a105fa86dc2b883c5ae8bfbb99
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-17 11:40:21 +0200

    test

--- single-office.php
+++ single-office.php
@@ -8,11 +8,9 @@
 <section class="block block-text wrapped headline-links">
     <h2>Unser Büro</h2>
     <div class="content">
-        <?php the_field('beschreibung');?>
-        <div style="float:right;padding:1rem;background:#EEE">
+    <div style="float:right;padding:1rem;background:#EEE">
                             <h3><?php echo get_the_title($id);?></h3>
                             <p>
-                                Büro <?php echo get_field('stadt');?><br />
                                 <?php echo get_field('strasse');?><br />
                                 <?php echo get_field('postleitzahl');?> <?php echo get_field('stadt');?>
                             </p>
@@ -25,6 +23,8 @@
                                 E-Mail: <a href="mailto:<?php echo get_field('email');?>"><?php echo get_field('email');?></a>
                             </p>
     </div>
+        <?php the_field('beschreibung');?>
+       
     </div>
 
 </section>
