a10efeb1acac2df9e2947d539f7684e836daffeb
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-17 11:38:46 +0200

    test

--- single-office.php
+++ single-office.php
@@ -5,8 +5,8 @@
         <h1><?php echo get_the_title();?></h1>
     </div>
 </section>
-<section class="block block-text wrapped <?php  if(get_field('headline_links')) { echo 'headline-links';} ?>">
-    <h2><?php the_field('headline');?></h2>
+<section class="block block-text wrapped headline-links">
+    <h2>Unser Büro</h2>
     <div class="content">
         <?php the_field('beschreibung');?>
     </div>
