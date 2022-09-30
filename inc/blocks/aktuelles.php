560ba41847ea04e3419ff80a13e40400ac07d039
Magnus Westhofen <m.westhofen@12-05.de>
2022-09-30 12:03:02 +0200

    button news

--- inc/blocks/aktuelles.php
+++ inc/blocks/aktuelles.php
@@ -11,7 +11,7 @@
                 <a class="post" href="<?php echo get_permalink($post->ID);?>">
                     <div class="date"><?php echo get_the_date('d.m.Y', $post->ID);?></div>
                     <h3><?php echo get_the_title($post->ID);?></h3>
-                    <div class="excerpt"><?php echo get_the_excerpt($post->ID);?></div>
+                    <div class="button">Mehr erfahren<svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg></div>
                 </a>
             <?php }
         }
