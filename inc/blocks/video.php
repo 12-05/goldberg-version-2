0c03081ed4c9270065dd5d47fd652f5399a44e29
Magnus Westhofen <m.westhofen@12-05.de>
2022-09-30 16:14:54 +0200

    test

--- inc/blocks/video.php
+++ inc/blocks/video.php
@@ -1,5 +1,5 @@
 <section class="block block-video">
-<video  id="hero_video" style="z-index: 0;"  autoplay muted loop ><source src="<?php the_field('video');?>" type="video/mp4"></video>
+<video  id="hero_video" style="z-index: 0;"  autoplay muted loop playsinline ><source src="<?php the_field('video');?>" type="video/mp4"></video>
     <div class="wrapped">
         <h1><?php the_field('headline');?></h1>
     </div>
