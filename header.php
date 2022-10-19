b2437d96e7e0292c6701d17445bebd6d823eb4b2
Magnus Westhofen <m.westhofen@12-05.de>
2022-10-19 17:37:29 +0200

    test

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <html>
     <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+
         <?php wp_head();?>
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
         <!--- display wordpress title tag -->
         <title><?php wp_title('|', true, 'right'); ?> <?php echo get_bloginfo('name');?></title>
