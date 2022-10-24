1d7cb53dc154d423c5e6eac0060d1ba4aaf2b384
Magnus Westhofen <m.westhofen@12-05.de>
2022-10-24 14:57:05 +0200

    test

--- inc/blocks/video.php
+++ inc/blocks/video.php
@@ -3,4 +3,10 @@
     <div class="wrapped">
         <h1><?php the_field('headline');?></h1>
     </div>
+    <?php 
+    if(get_field('home_siegel', 'option')) {?>
+        <div class="home-siegel">
+            <img src="<?php the_field('home_siegel', 'option');?>" alt="Siegel" />
+        </div>
+    <?php } ?>     
 </section>
