4c2c2f3a6b626c83ff8dee176852ff9bd13ab774
Magnus Westhofen <m.westhofen@12-05.de>
2023-06-27 11:54:58 +0200

    test

--- functions.php
+++ functions.php
@@ -1,104 +1,143 @@
-<?php 
+<?php
 
-    class GoldBergThemeClass {
-        public function __construct() {
-            add_action( 'wp_enqueue_scripts', array($this, 'load_styles'));
-            add_action( 'wp_enqueue_scripts', array($this, 'load_scripts'));
-            add_filter('acf/settings/save_json', array($this, 'save_fields'));
-            add_filter('acf/settings/load_json', array($this, 'load_fields'));
-            add_action('init', array($this, 'add_nav_menus'));
-            add_action('acf/init', array($this, 'register_options_page'));
-            add_filter( 'show_admin_bar', '__return_false' );
-            add_action( 'after_setup_theme', array($this, 'add_editor_styles'));
-            add_action('widgets_init', array($this, 'register_side_bar'));
-            add_action( 'enqueue_block_editor_assets', array($this, 'load_editors_scripts'));
-          
-            add_filter( 'excerpt_length', array($this, 'excerpt_length'), 999 );
-            $this->load_inc();  
-        }
+class GoldBergThemeClass
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'load_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'load_scripts'));
+        add_filter('acf/settings/save_json', array($this, 'save_fields'));
+        add_filter('acf/settings/load_json', array($this, 'load_fields'));
+        add_action('init', array($this, 'add_nav_menus'));
+        add_action('acf/init', array($this, 'register_options_page'));
+        add_filter('show_admin_bar', '__return_false');
+        add_action('after_setup_theme', array($this, 'add_editor_styles'));
+        add_action('widgets_init', array($this, 'register_side_bar'));
+        add_action('enqueue_block_editor_assets', array($this, 'load_editors_scripts'));
+        add_action('acf/init', array($this, 'add_news_fields'));
+        add_filter('excerpt_length', array($this, 'excerpt_length'), 999);
+        add_theme_support('post-thumbnails');
+        $this->load_inc();
+    }
 
-        public function load_inc() {
-            require_once get_stylesheet_directory().'/inc/lawyer.model.php';
-            require_once get_stylesheet_directory().'/inc/office.model.php';
-            require_once get_stylesheet_directory().'/inc/field.model.php';
-            require_once get_stylesheet_directory().'/inc/misc.functions.php';
-            require_once get_stylesheet_directory().'/inc/blocks.class.php';
-            require_once get_stylesheet_directory().'/inc/siegel.php';
-            require_once get_stylesheet_directory().'/misc.php';
-
-
-        }
-        
-        public function load_styles() {
-            wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/compiled/style.css' );
-            wp_enqueue_style( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css' );
-            wp_enqueue_style( 'slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css' );
-
-        }
-
-
-        public function load_scripts() {
-            wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/compiled/custom.js', array('jquery') );
-            wp_enqueue_script( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery') );
-        }
-
-
-        public function save_fields() {
-            $path = get_stylesheet_directory() . '/inc/fields';
-            return $path;
-        }
-
-        public function load_fields($paths) {
-            $paths[] = get_stylesheet_directory() . '/inc/fields';
-            return $paths;
-        }
-
-        public function add_nav_menus() {
-            register_nav_menus( array(
-                'main'=>'Hauptmenue',
-                'footer'=> 'Footermenue',
-            ));
-        }
-
-        public function register_options_page() {
-            acf_add_options_page(array(
-                'page_title' 	=> 'Goldberg Theme Einstellungen',
-                'menu_title'	=> 'Goldberg',
-                'menu_slug' 	=> 'goldgberg-theme-settings',
-                'capability'	=> 'manage_options',
-                'redirect'		=> false
-            ));           
-        }
-
-        public function add_editor_styles() {
-            add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added
-
-            add_editor_style( get_template_directory_uri().'/assets/css/compiled/style.css' ); // tries to include style-editor.css directly from your theme folder
-        }
-
-        public function load_editors_scripts() {
-            wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/compiled/custom.js', array('jquery') );
-            wp_enqueue_script( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery') );
-        }
-        public function register_side_bar() {
-            register_sidebar( array(
-                'name'          => __( 'Sidebar2', 'goldberg' ),
-                'id'            => 'goldsidebar',
-                'description'   => __( 'Add widgets here to appear in your sidebar.', 'goldberg' ),
-                'before_widget' => '<section id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</section>',
-                'before_title'  => '<h2 class="widget-title">',
-                'after_title'   => '</h2>',
-            ) );
-        }
-
-        public function excerpt_length() {
-            return 20;
-        }
-
+    public function load_inc()
+    {
+        require_once get_stylesheet_directory() . '/inc/lawyer.model.php';
+        require_once get_stylesheet_directory() . '/inc/office.model.php';
+        require_once get_stylesheet_directory() . '/inc/field.model.php';
+        require_once get_stylesheet_directory() . '/inc/misc.functions.php';
+        require_once get_stylesheet_directory() . '/inc/blocks.class.php';
+        require_once get_stylesheet_directory() . '/inc/siegel.php';
+        require_once get_stylesheet_directory() . '/misc.php';
 
     }
 
-    new GoldBergThemeClass();
+    public function load_styles()
+    {
+        wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/compiled/style.css');
+        wp_enqueue_style('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
+        wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css');
 
+    }
 
+    public function load_scripts()
+    {
+        wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/compiled/custom.js', array('jquery'));
+        wp_enqueue_script('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'));
+    }
+
+    public function save_fields()
+    {
+        $path = get_stylesheet_directory() . '/inc/fields';
+        return $path;
+    }
+
+    public function load_fields($paths)
+    {
+        $paths[] = get_stylesheet_directory() . '/inc/fields';
+        return $paths;
+    }
+
+    public function add_nav_menus()
+    {
+        register_nav_menus(array(
+            'main' => 'Hauptmenue',
+            'footer' => 'Footermenue',
+        ));
+    }
+
+    public function register_options_page()
+    {
+        acf_add_options_page(array(
+            'page_title' => 'Goldberg Theme Einstellungen',
+            'menu_title' => 'Goldberg',
+            'menu_slug' => 'goldgberg-theme-settings',
+            'capability' => 'manage_options',
+            'redirect' => false,
+        ));
+    }
+
+    public function add_editor_styles()
+    {
+        add_theme_support('editor-styles'); // if you don't add this line, your stylesheet won't be added
+
+        add_editor_style(get_template_directory_uri() . '/assets/css/compiled/style.css'); // tries to include style-editor.css directly from your theme folder
+    }
+
+    public function load_editors_scripts()
+    {
+        wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/compiled/custom.js', array('jquery'));
+        wp_enqueue_script('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'));
+    }
+    public function register_side_bar()
+    {
+        register_sidebar(array(
+            'name' => __('Sidebar2', 'goldberg'),
+            'id' => 'goldsidebar',
+            'description' => __('Add widgets here to appear in your sidebar.', 'goldberg'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>',
+        ));
+    }
+
+    public function excerpt_length()
+    {
+        return 20;
+    }
+    // add local field group for posts where you select the post object anwalt
+
+    public function add_news_fields()
+    {
+        acf_add_local_field_group(array(
+            'key' => 'group_news_int',
+            'title' => 'Anwaltzuordnung',
+            'fields' => array(
+                array(
+                    'key' => 'news_field_anwalt_123',
+                    'name' => 'anwalt',
+                    'label' => 'Anwalt',
+                    'type' => 'post_object',
+                    'post_type' => array(
+                        0 => 'anwalt',
+                    ),
+                    'return_format' => 'id',
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'post',
+                    ),
+                ),
+            ),
+        )
+        );
+    }
+
+}
+
+new GoldBergThemeClass();
--- inc/templates/single.php
+++ inc/templates/single.php
@@ -6,26 +6,36 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
-<article <?php post_class(' goldberg-post'); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class(' goldberg-post');?> id="post-<?php the_ID();?>">
 
 	<header class="entry-header">
-    <?php if ( 'post' === get_post_type() ) : ?>
+
+	<!-- check if post thumbnail exists and render it -->
+	<?php if (has_post_thumbnail()): ?>
+		<a href="<?php echo get_permalink(); ?>" class="post-thumbnail">
+
+			<?php the_post_thumbnail('large', ['class' => 'img-fluid']);?>
+
+		</a>
+	<?php endif;?>
+
+    <?php if ('post' === get_post_type()): ?>
 
             <div class="entry-meta">
 				<?php echo get_the_date('d.m.Y'); ?>
 			</div><!-- .entry-meta -->
 
-		<?php endif; ?>
+		<?php endif;?>
 
 		<?php
-		the_title(
-			sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h1>'
-		);
-		?>
+the_title(
+    sprintf('<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+    '</a></h1>'
+);
+?>
 
 
 	</header><!-- .entry-header -->
@@ -34,19 +44,33 @@ defined( 'ABSPATH' ) || exit;
 	<div class="entry-content">
 
 		<?php
-		the_content();
-		?>
-        
+the_content();
+?>
+
 
 	</div><!-- .entry-content -->
+	<?php if (get_field('anwalt')) {
+    $anwalt = get_field('anwalt');
+    ?>
+		<div class="anwalt-footer">
+			<a href="<?php echo get_permalink($anwalt); ?>"><img src="<?php the_field('bild', $anwalt);?>" alt="<?php echo get_the_title($anwalt); ?>" class="anwalt-bild"></a>
+			<div class="anwalt-text">
+				<a href="<?php echo get_permalink($anwalt); ?>"><h3 class="anwalt-name"><?php echo get_the_title($anwalt); ?></h3></a>
+				<p class="anwalt-position"><?php the_field('titel', $anwalt);?></p>
+				<p class="anwalt-telefon"><?php the_field('telefon', $anwalt);?><br />
+				<a href="mailto:<?php the_field('email', $anwalt);?>">
 
+				<?php the_field('email', $anwalt);?></a></p>
+	</div>
+		</div>
+	<?php }?>
 	<footer class="entry-footer">
-        <?php 
-            // make next and previous post navigation
-            the_post_navigation( array(
-                'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Vorheriger Beitrag'),
-                'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Nächster Beitrag'),
-            ) );?>
+        <?php
+// make next and previous post navigation
+the_post_navigation(array(
+    'prev_text' => '<span class="nav-subtitle">' . esc_html__('Vorheriger Beitrag'),
+    'next_text' => '<span class="nav-subtitle">' . esc_html__('Nächster Beitrag'),
+));?>
 
 	</footer><!-- .entry-footer -->
 
