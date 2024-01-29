80ea6372f6bbf0a33731c73fce8799b2e6a6dc28
Magnus Westhofen <m.westhofen@12-05.de>
2024-01-29 14:51:18 +0100

    Update misc.functions.php

--- inc/misc.functions.php
+++ inc/misc.functions.php
@@ -3,3 +3,10 @@
         $url = get_stylesheet_directory_uri().'/assets/images/'.$fileName;
         echo $url;
     }
+
+add_filter( 'acf/shortcode/allow_unsafe_html', function ( $allowed, $atts ) {
+    if ( $atts['field'] === 'content' ) {
+        return true;
+    }
+    return $allowed;
+}, 10, 2 );
