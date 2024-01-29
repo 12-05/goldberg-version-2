92994f995c759f463e50bf5a6b7b3e894c3ccc3d
Magnus Westhofen <m.westhofen@12-05.de>
2024-01-29 14:47:10 +0100

    Update text.php

--- inc/blocks/text.php
+++ inc/blocks/text.php
@@ -1,7 +1,7 @@
 <section class="block block-text wrapped <?php  if(get_field('headline_links')) { echo 'headline-links';} ?>">
     <h2><?php the_field('headline');?></h2>
     <div class="content">
-        <?php the_field('content');?>
+        <?php echo get_field('content');?>
     </div>
 </section>
 
