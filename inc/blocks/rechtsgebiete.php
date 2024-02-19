552d5929a2b01bc4acf602c42be84894c581a04e
Magnus Westhofen <m.westhofen@12-05.de>
2024-02-19 15:56:29 +0100

    test

--- inc/blocks/rechtsgebiete.php
+++ inc/blocks/rechtsgebiete.php
@@ -3,12 +3,12 @@
         <h2><?php the_field('headline');?></h2>
         <div class="grid">
         <?php $rechtsgebiete = get_field('rechtsgebiete');
-            if($rechtsgebiete) {
-                foreach($rechtsgebiete as $rechtsgebiet) {
-                    ?>
-                        <a href="<?php echo get_permalink($rechtsgebiet);?>" class="rechtsgebiet">
+if ($rechtsgebiete) {
+    foreach ($rechtsgebiete as $rechtsgebiet) {
+        ?>
+                        <a href="<?php echo get_permalink($rechtsgebiet); ?>" class="rechtsgebiet">
                             <div class="text">
-                            <span class="label"><?php  echo get_the_title($rechtsgebiet);?></span>
+                            <span class="label"><?php echo get_the_title($rechtsgebiet); ?></span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46.557" height="22.827" viewBox="0 0 46.557 22.827">
                                     <g id="surface6499" transform="translate(45.191 1.413) rotate(90)">
                                         <path id="Pfad_5" data-name="Pfad 5" d="M10,1.067V44.191M20,9.3,10,0,0,9.3" transform="translate(0 0)" fill="none" stroke="#19335a" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"/>
@@ -16,10 +16,16 @@
                                 </svg>
                             </div>
                         </a>
-                    <?php 
-                }
-            }
-        ?>
+                    <?php
+}
+}
+?>
         </div>
     </div>
+    <?php if (is_front_page()) {?>
+        <a href="<?php echo get_permalink(get_page_by_path('rechtsgebiete')); ?>" class="link">
+            <div class="text">
+                <span>Alle Rechtsgebiete</span>
+            </div>
+    <?php }?>
 </section>
