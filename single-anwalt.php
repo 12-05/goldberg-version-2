cf94091f6f29603da28ab11a5fab4dd2cd7c163d
Magnus Westhofen <m.westhofen@12-05.de>
2025-06-03 11:45:57 +0200

    test

--- single-anwalt.php
+++ single-anwalt.php
@@ -26,12 +26,12 @@
                              // make a download button with download icon inside for vita_file field
                                 $vita_file = get_field('vita_file');
                                 if($vita_file) {
-                                    echo '<a href="'.$vita_file.'" class="download-button">';
+                                    echo '<a name="Download" href="'.$vita_file.'" class="download-button">';
                                     echo '<span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#666" d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg></span>';
                                     echo '</a>';
                                 }
                                 // make a print button with print icon inside that prints the current page
-                                echo '<a href="javascript:window.print()" class="print-button">';
+                                echo '<a name="Print" href="javascript:window.print()" class="print-button">';
                                 echo '<span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#666" d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/></svg></span>';
                                 echo '</a>';
 
@@ -49,7 +49,7 @@ EMAIL;WORK:'.get_field('email').'
 END:VCARD';
 $vcard_file = 'anwalt-'.get_the_title().'.vcf';
                                                 
-                                echo '<a href="data:text/vcard;charset=utf-8,'.rawurlencode($vcard).'" download="'.$vcard_file.'" class="download-button">';
+                                echo '<a name="VCard" href="data:text/vcard;charset=utf-8,'.rawurlencode($vcard).'" download="'.$vcard_file.'" class="download-button">';
                                 echo '<span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#666" d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg></span>';
                                 echo '</a>';
 
