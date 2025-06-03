e385eab7fdcd07b379a950d12d1180eec9a9e6a1
Magnus Westhofen <m.westhofen@12-05.de>
2025-06-03 11:16:56 +0200

    test

--- inc/blocks/text.php
+++ inc/blocks/text.php
@@ -1,5 +1,7 @@
 <section class="block block-text wrapped <?php  if(get_field('headline_links')) { echo 'headline-links';} ?>">
-    <h2><?php the_field('headline');?></h2>
+    <?php if(get_field('headline')) { ?>     
+<h2><?php the_field('headline');?></h2>
+<?php } ?>
     <div class="content">
         <?php echo get_field('content');?>
     </div>
