9bf02a2e22cbe7dc531045fa9375d375c4246f50
Magnus Westhofen <m.westhofen@12-05.de>
2025-06-03 10:05:24 +0200

    test

--- inc/blocks/rechtsgebiete.php
+++ inc/blocks/rechtsgebiete.php
@@ -10,7 +10,7 @@ if ($rechtsgebiete) {
                             <div class="text">
                             <span class="label"><?php echo get_the_title($rechtsgebiet); ?></span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46.557" height="22.827" viewBox="0 0 46.557 22.827">
-                                    <g id="surface6499" transform="translate(45.191 1.413) rotate(90)">
+                                    <g  transform="translate(45.191 1.413) rotate(90)">
                                         <path data-name="Pfad 5" d="M10,1.067V44.191M20,9.3,10,0,0,9.3" transform="translate(0 0)" fill="none" stroke="#19335a" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"/>
                                     </g>
                                 </svg>
