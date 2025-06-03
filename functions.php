ed76d13bd5e971c8dcdbd80ada910014d23192db
Magnus Westhofen <m.westhofen@12-05.de>
2025-06-03 10:50:56 +0200

    Test

--- functions.php
+++ functions.php
@@ -150,7 +150,6 @@ class GoldBergThemeClass
     if ( function_exists( 'weglot_get_current_language' ) && function_exists( 'weglot_get_languages_available' ) ) {
         $current_lang = weglot_get_current_language();
         $languages = weglot_get_languages_available();
-        var_dump($languages);
         return;
         echo '<div class="country-selector">';
         foreach ( $languages as $language ) {
